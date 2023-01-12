da92f9481010b0d48fab1b0020acb5f61950b450
Kotaro Fujisawa <kotaro.fujisawa@gmail.com>
2023-01-12 22:06:00 +0900

    calculate fluid star

--- test_mountain.jl
+++ test_mountain.jl
@@ -1,6 +1,7 @@
 using Plots
 using DifferentialEquations
 using Dierckx
+using QuadGK
 
 function back_ground(du, u, param, r)
     G = 6.6743e-8
@@ -25,18 +26,6 @@ function affect!(integrator)
     terminate!(integrator)
 end
 
-function Poisson(du, u, param, r)
-    G = 6.6743e-8
-
-    δϕ   = u[1]
-    δϕ_dr= u[2]
-
-    δρ = 1.0e12
-    β2 = 2*(2+1)
-    du[1] = u[2]
-    du[2] = -2/r*u[2] + β2/r^2*u[1] + 4π*G*δρ
-end
-
 function main()
     #simple NS model
     Γ = 2
@@ -44,11 +33,18 @@ function main()
     M0 = 1.989e33 #g
     G = 6.6743e-8
     param = (Γ, K)
+    ℓ  = 2
+    β2 = ℓ*(ℓ+1)
 
-    ρ0 = 2.0e15
+    ρ0 = 2.0e15   #central density     r = r_min
+    ρc = 2.0e14   #core-crust boundary r = r_c
+    ρo = 1.0e11   #crust-ocian boundary r = r_o 
     p0 = K*ρ0^Γ
     m0 = 0.0
-    r_min = 0.1
+
+    r_min = 0.1   #[cm]
+    r_c = 0.0
+    r_o = 0.0
     f0 = [m0, p0]
     rspan = (r_min, 2.0e6)
     prob0 = ODEProblem(back_ground, f0, rspan, param)
@@ -63,6 +59,11 @@ function main()
     ρ = zeros(Float64, nr)
     p = zeros(Float64, nr)
     m = zeros(Float64, nr)
+
+    δρ = zeros(Float64, nr)
+    δϕ = zeros(Float64, nr)
+    dδϕ_dr = zeros(Float64, nr)
+
     for i=1:nr
         r[i] = bg_star.t[i]
         p[i] = bg_star.u[i][2]
@@ -90,6 +91,18 @@ function main()
 
     #interpolation by spline curve
     ρ_r = Spline1D(r, ρ)
+ 
+    r_c = 0.8e6
+    for i=1:10
+        r_c -= (ρ_r(r_c) - ρc) / derivative(ρ_r, r_c;nu=1)
+    end
+    r_o = 0.8e6
+    for i=1:10
+        r_o -= (ρ_r(r_o) - ρo) / derivative(ρ_r, r_o;nu=1)
+    end
+
+    print("r_c = $r_c  \n")
+    print("r_o = $r_o  \n")
 
     #grid point
     ng = 1000
@@ -104,30 +117,101 @@ function main()
     end
 ###
 
-###force
+### fluid star
 
-rspan   = (r_min, R)
-rspan_r = (R, R/2)
-fp = [0.0, -1.35335e6]
+    #force f_i = -Aρ ∇_i (r^2 Y_lm)
+    A = 1e9
+    fr(r) = -2A*r*ρ_r(r)
+    ft(r) =  -A*r*ρ_r(r)
 
-param = ()
-probP = ODEProblem(Poisson, fp, rspan, param)
-ppo = solve(probP, Tsit5(), reltol = 1.0e-8, abstol = 1.0e-8)
+    ###perturbe Poisson eq
+    rspan   = (r_min, R)
+    param = ()
+    δρ_r = Spline1D(r, δρ)
 
-fpr= [-1.0e13, +1.0e13/R*3]
+    function Poisson(du, u, param, r)
+#        δϕ   = u[1]
+#        δϕ_dr= u[2]
+    
+        β2 = 2*(2+1)
+        du[1] = u[2]
+        du[2] = -2/r*u[2] + β2/r^2*u[1] + 4π*G*δρ_r(r)
+#        tmp = δρ_r(r)
+#        print("r = $r, δρ = $tmp \n")
+    end
 
-probPr = ODEProblem(Poisson, fpr, rspan_r, param)
-ppor = solve(probPr, Tsit5(), reltol = 1.0e-8, abstol = 1.0e-8)
+    max_ite = 10
+    #initial guess
+    dδϕ_dr_c = -1.0e6
+    for ite=1:max_ite
+        
+        for i=1:nr
+            δρ[i] = -(ρ[i]*δϕ[i] - ft(r[i])) / cs2[i]
+        end
+        δρ_r = Spline1D(r, δρ)
+        ϵ    = 1.0e-6
+        S_int(r) = 4π*G*δρ_r(r) * 10r_min^(ℓ) * r^(-ℓ+1)
+        S = quadgk(S_int, 10r_min, R)
+        dδϕ_dr_c = -(S[1] ) / (10r_min)
+#        print(dδϕ_dr_c, " \n")
+        for j=1:10000
+        #shooting
+            f0 = [0.0, dδϕ_dr_c]
 
-#plot(ppor)
+            probP = ODEProblem(Poisson, f0, rspan, param)
+            ppo = solve(probP, Tsit5(), reltol = 1.0e-8, abstol = 1.0e-8)
+        
+            np = size(ppo.t, 1)
+            #dδϕ/dr + (ℓ+1)/R δϕ = 0 at r = R 
+            #-> f = -dδϕ/dr  /  (ℓ+1)/R * δϕ
+            f = -ppo.u[np][2] / ((ℓ+1) / (R) * ppo.u[np][1])
+            f -= 1.0
+            f0 = [0.0, dδϕ_dr_c*(1.0 + ϵ)]
+            #shooting
+            probP = ODEProblem(Poisson, f0, rspan, param)
+            ppo = solve(probP, Tsit5(), reltol = 1.0e-8, abstol = 1.0e-8)
+        
+            np = size(ppo.t, 1)
+            #dδϕ/dr + (ℓ+1)/R δϕ = 0 at r = R 
+            #-> f = -dδϕ/dr  /  (ℓ+1)/R * δϕ
+            f2 = -ppo.u[np][2] / ((ℓ+1) / (R) * ppo.u[np][1])
+            f2 -= 1.0
+            #Newton method
+            fp = (f2 - f) / ϵ
+            dδϕ_dr_c = dδϕ_dr_c - 1.0e3 * f / fp
 
-np = size(ppo.t, 1) - 1
+            if ite == 10000
+#                print("j = $j dδϕ_dr_c = $dδϕ_dr_c f = $f \n")
+            end
+            if(abs(f) < 1.0e-6) 
+#                print("j = $j dδϕ_dr_c = $dδϕ_dr_c f = $f \n")
+                break
+            end
+        end
+        f0 = [0.0, dδϕ_dr_c]
+        probP = ODEProblem(Poisson, f0, rspan, param)
+        ppo = solve(probP, Tsit5(), reltol = 1.0e-8, abstol = 1.0e-8)
+        for i = 1:nr
+            δϕ[i], dδϕ_dr[i] = ppo(r[i])
+        end
 
-print(ppo.u[np][1]," ", ppo.u[np][2], " ",-ppo.u[np][2]/ppo.u[np][1]*R/3,"\n")
+    end
 
-plot(ppo)
+    #quadrupole 
+    Q22(r) = δρ_r(r)*r^4
+
+    I0 = 1.0e45
+
+    S = quadgk(Q22, r_min, R)
+    ε_f = sqrt(8π/15) * S[1] / I0
+    print("ε_f = $ε_f \n")
+
+    plot(r, ρ)
 
 
-end 
+#solid crust
+    
+
+end
 
 main()
